2e15152dc923e7a028621f0e14a65370e28aa2d7
alexdng10 <hungqdang0510@gmail.com>
2025-01-18 07:50:18 -0500

    source code for phone app

---
+++ Phone/StepCounterTests/StepCounterTests.swift
@@ -0,0 +1,16 @@
+//
+//  StepCounterTests.swift
+//  StepCounterTests
+//
+//  Created by Alex Dang on 2025-01-17.
+//
+
+import Testing
+
+struct StepCounterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
